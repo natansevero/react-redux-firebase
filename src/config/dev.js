0068fe446bbe3f8bbe9dcab6acb4b9197061fb16
natansevero <natanseverolp@gmail.com>
2018-09-12 21:57:48 -0300

    finalizando aplicação

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBsMDdefr9FkJIyQwk3h9vByggOoGrcEYg",
+    authDomain: "reactreduxfirebase-fb860.firebaseapp.com",
+    databaseURL: "https://reactreduxfirebase-fb860.firebaseio.com",
+    projectId: "reactreduxfirebase-fb860",
+    storageBucket: "reactreduxfirebase-fb860.appspot.com",
+    messagingSenderId: "1055461735223"
+};
